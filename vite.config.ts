330fd338f26b40c67e2693a4e475a4983e7c9350
Art San <artsan.mcc@gmail.com>
2025-01-30 22:48:03 +0700

    end

--- vite.config.ts
+++ vite.config.ts
@@ -11,3 +11,17 @@ export default defineConfig({
     }
   }
 })
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import { fileURLToPath } from 'url'; // Добавьте этот импорт
+
+// export default defineConfig({
+//   plugins: [react()],
+
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//     },
+//   },
+// });
